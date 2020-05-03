3c438c3f2e21f0825ea1af1230c6a4ead9d5e6c4
flopreynat <flopreynat@protonmail.com>
2020-05-03 10:58:29 +0200

    rss feeds

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
                         date: edge.node.frontmatter.date,
                         url: site.siteMetadata.siteUrl + '/blog/' + edge.node.fields.slug,
                         guid: site.siteMetadata.siteUrl + '/blog/' + edge.node.fields.slug,
-                        custom_elements: [{ "content:encoded": edge.node.excerpt }],
+                        custom_elements: [{ "content:encoded": edge.node.html }],
                       })
                     })
                   },
@@ -85,6 +85,7 @@ module.exports = {
                         edges {
                           node {
                             excerpt
+                            html
                             fields { slug }
                             frontmatter {
                               title
