dc9bb1e739a7826dcfbe8a1952f4c0a1422575b2
flopreynat <flopreynat@protonmail.com>
2020-08-09 12:12:01 +0200

    removing contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,16 +10,7 @@ const ContactPage = () => {
             <div className="mainSection">
                 <h3>Contact</h3>
                 <p>Want to talk about code, mentorship, and more?</p>
-                <p>Ping me on Twitter via <a href="https://twitter.com/@flopreynat" target="_blank">@flopreynat</a> or use the below contact form:</p>
-                <form action="https://formspree.io/xknqngdz" method="POST">
-                    <input type="text" name="name" placeholder="Your Name" />
-                    <br />
-                    <input type="email" name="_replyto" placeholder="Your Email" />
-                    <br />                    
-                    <textarea rows="5" type="text" name="message" placeholder="Message..." />
-                    <br />
-                    <input type="submit" value="Send" />
-                </form>
+                <p>Ping me on Twitter via <a href="https://twitter.com/@flopreynat" target="_blank">@flopreynat</a>.</p>
             </div>
         </Layout>
     )
